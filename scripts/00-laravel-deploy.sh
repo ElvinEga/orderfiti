c8bdea6d28dd1725252775b113a563017a6ae896
Elvin Ega <proega3@gmail.com>
2024-06-18 13:16:43 +0300

    update docker

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 echo "Running composer"
+composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
+
+echo "generating application key..."
+php artisan key:generate --show
 
 echo "Caching config..."
 php artisan config:cache
@@ -8,18 +12,5 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
-php artisan optimize
-
-php artisan storage:link
-
-npm run dev
-
-
-
-
-
-#echo "Running migrations..."
-#php artisan migrate --force
-#
-#echo "Publishing cloudinary provider..."
-#php artisan vendor:publish --provider="CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider" --tag="cloudinary-laravel-config"
+echo "Running migrations..."
+php artisan migrate --force
